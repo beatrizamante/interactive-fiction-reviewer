9cbd8bd60f8215005c0af3ff00a7913ddc5ae71e
beatrizamante <beatrizamante@hotmail.com>
2026-04-06 15:49:30 -0300

    All tests fixed

--- backend/vitest.config.ts
+++ backend/vitest.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    exclude: ['**/node_modules/**', 'test/integration/db/**'],
+    include: ['src/**/*.spec.ts'],
+    exclude: ['**/node_modules/**'],
   },
 });
